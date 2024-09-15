baa2534b9e2d992d5ac7320f110d97563be8ee1e
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-15 21:06:04 +0200

    constant value

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	var annualInterestRate = 5.5
 	var years float64 = 10
 
 	var futureInvestmentValue = investmentAmount * math.Pow(1+annualInterestRate/100, years)
+	futureRealValue := futureInvestmentValue / math.Pow(1+inflationRate/100, years)
 	fmt.Print("Future value is ", futureInvestmentValue)
+	fmt.Print("\nFuture real value is ", futureRealValue)
 
 }
